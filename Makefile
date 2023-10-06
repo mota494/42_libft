d3932a49eb1c5ec2927e82b76a6cb57641b9a756
Mota494 <migueloureiromota@gmail.com>
2023-10-06 10:24:07 +0100

    created gitignore and solved ft_strlcat

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 NAME = libft.a
 CC= gcc -g
 FLAGS= -Wall -Wextra -Werror
-SRC= ft_bzero.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_memcpy.c ft_memmove.c ft_memset.c ft_strlcpy.c ft_strlen.c
+SRC= ft_bzero.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_memcpy.c ft_memmove.c ft_memset.c ft_strlcat.c ft_strlcpy.c ft_strlen.c
 OBJS = $(SRC:.c=.o)
 RM = rm -rf
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mloureir <mloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 12:27:43 by mloureir          #+#    #+#             */
-/*   Updated: 2023/10/06 10:14:20 by mloureir         ###   ########.fr       */
+/*   Updated: 2023/10/06 10:20:36 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ void ft_bzero(void *s, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
 size_t ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst,const char *src, size_t size);
 
 #endif
