e6b7c9778c37caf2bfce127bf16ab9dbce074053
Mota494 <migueloureiromota@gmail.com>
2023-10-06 16:11:01 +0100

    fixed norminette errors on strncmp and memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,35 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mloureir <mloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/06 12:48:10 by mloureir          #+#    #+#             */
-/*   Updated: 2023/10/06 15:15:06 by mloureir         ###   ########.fr       */
+/*   Created: 2023/10/06 14:56:59 by mloureir          #+#    #+#             */
+/*   Updated: 2023/10/06 16:10:46 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
 
 	i = 0;
-	if (n == 0)
-		return (0);
-	while (s1[i] == s2[i] && i < n - 1 && s1[i] != '\0')
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
 }
 // #include <stdio.h>
 // #include <string.h>
+
 // int main()
 // {
-// 	char *s1 = "\200";
-// 	char *s2 = "\0";
-
-// 	printf("%d\n", ft_strncmp(s1, s2, 6));
-// 	printf("%d", strncmp(s1, s2, 6));
+// 	char str1[] = "LOREM_IPSUM";
+// 	char str2[] = "LOREM_IPSUM";
+// 	printf("%d\n", memcmp(str1, str2, 90));
+// 	printf("%d\n", ft_memcmp(str1, str2, 90));
 // }
