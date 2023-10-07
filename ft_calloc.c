f5f207d0a526e1c4dbc630469cb67837309dd341
mota494 <migueloureiromota@gmail.com>
2023-10-07 19:02:50 +0100

    Solved ft_calloc (not yet tested)

---
+++ ft_calloc.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+
+void    *ft_calloc(size_t nmemb, size_t size)
+{
+    void    *ptr,
+
+    *ptr = malloc(nmemb * size);
+    ft_bzero(ptr, nmemb * size);
+    return (ptr);
+}
