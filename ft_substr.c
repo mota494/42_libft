0d6ad8d94f8f10cbe4a3e25dd38e731eec00399e
Mota494 <migueloureiromota@gmail.com>
2023-10-09 12:29:51 +0100

    Redid ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mloureir <mloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 10:29:40 by mloureir          #+#    #+#             */
-/*   Updated: 2023/10/09 11:13:59 by mloureir         ###   ########.fr       */
+/*   Updated: 2023/10/09 12:28:24 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,30 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
-	size_t	strl;
 	char	*toret;
+	size_t	strl;
 
-	i = 0;
 	strl = ft_strlen(s);
-	if (len >= strl)
-		len = strl - start;
-	if (start >= strl)
+	if (start > strl)
+		return (ft_strdup(""));
+	else if (len + start > strl)
 	{
-		toret = (char *)ft_calloc(1, sizeof(char));
-		if (!toret)
-			return (NULL);
-		return (toret);
+		len = strl - start;
+		toret = ft_calloc(len + 1, sizeof(char));
 	}
-	toret = (char *)ft_calloc(len + 1, sizeof(char));
+	else
+		toret = ft_calloc(len + 1, sizeof(char));
 	if (!toret)
 		return (NULL);
-	while (i++ < len)
-		toret[i] = s[start + i];
+	ft_strlcpy(toret, s + start, len + 1);
 	return (toret);
 }
+
 // #include <stdio.h>
 // int main()
 // {
-// 	char *string = "LOREM_IPSUM";
-// 	char *temp = ft_substr(string, 6, 5);
+// 	char *string = "hola";
+// 	char *temp = ft_substr(string, 2, 3);
 // 	printf("%s\n", temp);
 
 // 	return (0);
