e20d816e126a77f1f48d0fbac27c04ff1fcfffc9
Mota494 <migueloureiromota@gmail.com>
2023-10-10 16:43:16 +0100

    Fixed ft_itoa norminette errors

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,10 +6,9 @@
 /*   By: mloureir <mloureir@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 12:46:51 by mloureir          #+#    #+#             */
-/*   Updated: 2023/10/10 15:32:44 by mloureir         ###   ########.fr       */
+/*   Updated: 2023/10/10 16:42:15 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libft.h"
 #include <stdio.h>
 
@@ -26,23 +25,24 @@ size_t	alocsize(int n)
 	return (i);
 }
 
+
+
 char	*ft_itoa(int n)
 {
 	char		*toret;
 	int			i;
 
 	i = alocsize(n);
-	if(n < 0)
-	{
-		if (!(toret = ft_calloc(alocsize(n) + 2, sizeof(char))))
-			return (NULL);
+	toret = ft_calloc(alocsize(n) + 1 + (n < 0), sizeof(char));
+	if (!toret)
+		return (NULL);
+	if (n < 0)
 		toret[0] = '-';
-	}
-	else if (n > 0)
-		if (!(toret = ft_calloc(alocsize(n) + 1, sizeof(char))))
-			return (NULL);
-	if(n == 0)
+	else if (n == 0)
+	{
+		free(toret);
 		toret = ft_strdup("0");
+	}
 	while(n != 0)
 	{
 		if(n < 0)
@@ -54,9 +54,10 @@ char	*ft_itoa(int n)
 	}
 	return (toret);
 }
+
 //int main()
 //{
-//	int n = 0;
+//	int n = -91;
 //	char *string;
 //	string = ft_itoa(n);
 //	printf("%s\n", string);
