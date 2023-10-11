69900a9fc890d612fc24b99f5980f505f284adf1
Mota494 <migueloureiromota@gmail.com>
2023-10-11 16:49:11 +0100

    Solved ft_lstsize.c and norm errors

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mloureir <mloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 12:27:43 by mloureir          #+#    #+#             */
-/*   Updated: 2023/10/11 16:42:05 by mloureir         ###   ########.fr       */
+/*   Updated: 2023/10/11 16:44:51 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,5 +57,6 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
 
 #endif
