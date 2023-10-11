308130705977aa3e0b72f8494e30cbed2a37c87d
Mota494 <migueloureiromota@gmail.com>
2023-10-11 12:59:34 +0100

    Solved ft_striteri.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mloureir <mloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 12:27:43 by mloureir          #+#    #+#             */
-/*   Updated: 2023/10/11 12:28:45 by mloureir         ###   ########.fr       */
+/*   Updated: 2023/10/11 12:47:43 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
