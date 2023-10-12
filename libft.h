6b30dfb2159e34e17632080e0e042783cbb0695e
Mota494 <migueloureiromota@gmail.com>
2023-10-12 11:47:10 +0100

    solved ft_lstiter

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mloureir <mloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 12:27:43 by mloureir          #+#    #+#             */
-/*   Updated: 2023/10/12 10:57:08 by mloureir         ###   ########.fr       */
+/*   Updated: 2023/10/12 11:10:40 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,4 +62,5 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 #endif
