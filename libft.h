462e97b32f0ceddbde2e42cb50a711f86e30217a
Mota494 <migueloureiromota@gmail.com>
2023-10-12 11:07:54 +0100

    solved ft_lstclear

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mloureir <mloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 12:27:43 by mloureir          #+#    #+#             */
-/*   Updated: 2023/10/12 09:54:24 by mloureir         ###   ########.fr       */
+/*   Updated: 2023/10/12 10:57:08 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,5 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
-
+void	ft_lstclear(t_list **lst, void (*del)(void*));
 #endif
